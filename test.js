71a43c2335611eeaf6228cc0c0b0ba129c59770e
Benz Muircroft (Am/is) <benzmuircroft@gmail.com>
2023-12-25 21:45:24 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+;(async function() {
+
+  const userKey = 'subNameString';
+  
+  const tinybee = require('tinybee')('folderName');
+
+  await tinybee.put('a', 0);
+  await tinybee.put('b', 'string');
+  await tinybee.put('c', { d: [0, 1, 2] });
+  
+  console.log(await tinybee.get('a')); // 0
+  console.log(await tinybee.get('b')); // 'string'
+  console.log(await tinybee.get('c')); // { d: [0, 1, 2] }
+  
+  // del
+  await tinybee.del('a');
+  
+  // using subs
+  await tinybee.put('a', 0, userKey);
+  console.log(await tinybee.get('a', userKey));
+  await tinybee.del('a', userKey);
+  
+  // get all entries as JSON object
+  await tinybee.get();
+  
+})();
