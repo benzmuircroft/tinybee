8ab393c2f9bcab89eff8d3180d0f2d0888c8e299
Benz Muircroft (Am/is) <benzmuircroft@gmail.com>
2024-01-29 23:31:13 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ const tinybee = async (_options) => { // self-invoking function
       }
       
       const debug = options.debug;
+      console.log('debug:', debug);
       const key = options.key;
       const keyPair = options.keyPair;
-      const backup = options.backup;
-  
-      delete options.backup;
+      
       delete options.folderNameOrCorestore;
       delete options.debug;
       delete options.key;
@@ -34,24 +33,35 @@ const tinybee = async (_options) => { // self-invoking function
   
       if (!keyPair) {
         if (debug) console.log('read only core');
-        input = store.get({ sparse: false, key });
+        input = store.get({ key });
         await input.ready();
         db = new Hyperbee(input);
         await db.ready();
       }
       else {
-        input = store.get({ sparse: false, keyPair });
+        input = store.get({ keyPair });
         await input.ready();
         if (input.length) {
-          
+          if (debug) console.log('removing cores history ...');
           db = new Hyperbee(input);
           await db.ready();
           const trim = db.core.length;
           const view = db.createReadStream();
           for await (const entry of view) {
-            await db.put(entry.key.toString(), entry.value.toString());
+            let key = entry.key.toString();
+            if (key.includes('\x00')) {
+              key = key.split('\x00')[1];
+            }
+            const value = entry.value.toString();
+            await db.put(key, value);
           }
-          await db.core.clear(1, trim);
+          await db.core.clear(0, trim);
+          if (debug) {
+            console.log('removed');
+            await db.put('debug', '0');
+            console.log('debug should be 0:', await db.get('debug'));
+            await db.del('debug');
+          }
         }
         else {
           if (debug) console.log('fresh core');
@@ -60,73 +70,9 @@ const tinybee = async (_options) => { // self-invoking function
         }
       }
   
-      let mem = {
-        subs: {},
-        main: {
-          subs: []
-        }
-      };
-      
-      if (backup) {
-        try {
-          await fs.stat(`./db/${backup.file}.json`);
-        } catch (e) {
-          await fs.writeFile(`./db/${backup.file}.json`, backup.encryptor.en(JSON.stringify(mem)), 'utf-8');
-        }
-      }
-  
-      const paper = {
-        batch: async function(array, sub) {
-          if (sub) {
-            if (!mem.subs[sub]) {
-              mem.subs[sub] = {};
-              mem.main.subs.push(sub);
-            }
-          }
-          for (const job of array) {
-            if (!Array.isArray(job) || (job[1] == 'put' && job.length != 3) || (job[1] == 'del' && job.length != 2)) {
-              throw new Error(`Malformed batch at ${JSON.stringify(job)}`);
-            }
-            if (job[2] && typeof job[2] !== 'string') job[2] = JSON.stringify(job[2]);
-            if (sub) {
-              if (job[0] == 'put') mem.subs[sub][job[1]] = job[2];
-              else if (job[1] == 'del') delete mem.subs[sub][job[1]];
-            }
-            else if (job[0] == 'put') mem.main[job[1]] = job[2];
-            else if (job[1] == 'del') delete mem.main[job[1]];
-          }
-          await fs.writeFile(`./db/${backup.file}.json`, backup.encryptor.en(JSON.stringify(mem)), 'utf-8');
-        },
-        put: async function(k, v, sub) {
-          if (typeof v !== 'string') v = JSON.stringify(v);
-          if (sub) {
-            if (!mem.subs[sub]) {
-              mem.subs[sub] = {};
-              mem.main.subs.push(sub);
-            }
-            mem.subs[sub][k] = v;
-          }
-          else {
-            mem.main[k] = v;
-          }
-          await fs.writeFile(`./db/${backup.file}.json`, backup.encryptor.en(JSON.stringify(mem)), 'utf-8');
-        },
-        del: async function(k, sub) {
-          if (sub) {
-            delete mem.subs[sub][k];
-          }
-          else {
-            delete mem.main[k];
-          }
-          await fs.writeFile(`./db/${backup.file}.json`, backup.encryptor.en(JSON.stringify(mem)), 'utf-8');
-        }
-      };
-  
       tb = {
-        mem,
         db,
         batch: async function(array /*[[put/del, k, v],]*/, sub) {
-          if (backup) await paper.batch(array, sub);
           let batch;
           if (sub) {
             if (!((await db.get('subs')) || []).includes(sub)) await db.put('subs', sub);
@@ -148,7 +94,6 @@ const tinybee = async (_options) => { // self-invoking function
           await batch.flush();
         },
         put: async function(k, v, sub) {
-          if (backup) await paper.put(k, v, sub);
           if (typeof v !== 'string') v = JSON.stringify(v);
           if (sub) {
             if (!((await db.get('subs')) || []).includes(sub)) await db.put('subs', sub);
@@ -194,7 +139,6 @@ const tinybee = async (_options) => { // self-invoking function
           }
         },
         del: async function(k, sub) {
-          if (backup) await paper.del(k, sub);
           if (sub) {
             sub = db.sub(sub);
             await sub.del(k);
@@ -205,31 +149,7 @@ const tinybee = async (_options) => { // self-invoking function
         }
       }; // tb
   
-      if (backup) {
-        // reconstruct
-        mem.main = await tb.get();
-        if (!mem.main.subs) mem.main.subs = [];
-        for await (const sub of mem.main.subs) {
-          mem.main[sub] = await tb.get(undefined, sub);
-        }
-        console.log(mem);
-        if (Object.keys(mem.main).length == 1) {
-          mem = JSON.parse(backup.encryptor.de(await fs.readFile(`./db/${backup.file}.json`, 'utf-8')));
-          console.log(mem, JSON.stringify(mem).length);
-          if (JSON.stringify(mem).length > 30) {
-            for await (const [k, v] of mem.main) {
-              await tb.put(k, v);
-            }
-            for await (const sub of mem.main.subs) {
-              for await (const [k, v] of mem.subs[sub]) {
-                await tb.put(k, v, sub);
-              }
-            }
-          }
-        }
-      }
-  
-      if (!keyPair) {
+      if (!keyPair && false) {
         delete tb.batch;
         delete tb.put;
         delete tb.del;
